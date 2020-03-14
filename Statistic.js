bddef2190ed9835a4788441df8fbcbec0934898e
liuchenghua <547173318@qq.com>
2020-03-14 23:32:18 +0800

    Create Statistic.js

---
+++ Statistic.js
@@ -0,0 +1,611 @@
+    //这是安徽的数据
+    localStorage.安徽0119i="0";
+    localStorage.安徽0119s="0";
+    localStorage.安徽0119c="0";
+    localStorage.安徽0119d="0";
+
+    localStorage.安徽0120i="0";
+    localStorage.安徽0120s="0";
+    localStorage.安徽0120c="0";
+    localStorage.安徽0120d="0";
+
+    localStorage.安徽0121i="0";
+    localStorage.安徽0121s="1";
+    localStorage.安徽0121c="0";
+    localStorage.安徽0121d="0";
+
+    localStorage.安徽0122i="1";
+    localStorage.安徽0122s="3";
+    localStorage.安徽0122c="0";
+    localStorage.安徽0122d="0";
+
+    localStorage.安徽0123i="9";
+    localStorage.安徽0123s="3";
+    localStorage.安徽0123c="0";
+    localStorage.安徽0123d="0";
+
+    localStorage.安徽0124i="15";
+    localStorage.安徽0124s="3";
+    localStorage.安徽0124c="0";
+    localStorage.安徽0124d="0";
+
+    localStorage.安徽0125i="39";
+    localStorage.安徽0125s="29";
+    localStorage.安徽0125c="0";
+    localStorage.安徽0125d="0";
+
+    localStorage.安徽0126i="60";
+    localStorage.安徽0126s="44";
+    localStorage.安徽0126c="0";
+    localStorage.安徽0126d="0";
+
+    localStorage.安徽0127i="70";
+    localStorage.安徽0127s="80";
+    localStorage.安徽0127c="0";
+    localStorage.安徽0127d="0";
+
+    localStorage.安徽0128i="106";
+    localStorage.安徽0128s="118";
+    localStorage.安徽0128c="0";
+    localStorage.安徽0128d="0";
+
+    localStorage.安徽0129i="152";
+    localStorage.安徽0129s="160";
+    localStorage.安徽0129c="6";
+    localStorage.安徽0129d="0";
+
+    localStorage.安徽0130i="198";
+    localStorage.安徽0130s="205";
+    localStorage.安徽0130c="2";
+    localStorage.安徽0130d="0";
+
+    localStorage.安徽0131i="234";
+    localStorage.安徽0131s="275";
+    localStorage.安徽0131c="3";
+    localStorage.安徽0131d="0";
+
+    localStorage.安徽0201i="294";
+    localStorage.安徽0201s="352";
+    localStorage.安徽0201c="3";
+    localStorage.安徽0201d="0";
+
+    localStorage.安徽0202i="335";
+    localStorage.安徽0202s="434";
+    localStorage.安徽0202c="5";
+    localStorage.安徽0202d="0";
+
+    //这是北京的数据
+    localStorage.北京0119i="0";
+    localStorage.北京0119s="0";
+    localStorage.北京0119c="0";
+    localStorage.北京0119d="0";
+
+    localStorage.北京0120i="5";
+    localStorage.北京0120s="0";
+    localStorage.北京0120c="0";
+    localStorage.北京0120d="0";
+
+    localStorage.北京0121i="10";
+    localStorage.北京0121s="0";
+    localStorage.北京0121c="0";
+    localStorage.北京0121d="0";
+
+    localStorage.北京0122i="14";
+    localStorage.北京0122s="0";
+    localStorage.北京0122c="0";
+    localStorage.北京0122d="0";
+
+    localStorage.北京0123i="26";
+    localStorage.北京0123s="0";
+    localStorage.北京0123c="0";
+    localStorage.北京0123d="0";
+
+    localStorage.北京0124i="35";
+    localStorage.北京0124s="0";
+    localStorage.北京0124c="1";
+    localStorage.北京0124d="0";
+
+    localStorage.北京0125i="49";
+    localStorage.北京0125s="0";
+    localStorage.北京0125c="2";
+    localStorage.北京0125d="0";
+
+    localStorage.北京0126i="66";
+    localStorage.北京0126s="0";
+    localStorage.北京0126c="2";
+    localStorage.北京0126d="0";
+
+    localStorage.北京0127i="77";
+    localStorage.北京0127s="0";
+    localStorage.北京0127c="2";
+    localStorage.北京0127d="1";
+
+    localStorage.北京0128i="86";
+    localStorage.北京0128s="0";
+    localStorage.北京0128c="4";
+    localStorage.北京0128d="1";
+
+    localStorage.北京0129i="106";
+    localStorage.北京0129s="0";
+    localStorage.北京0129c="4";
+    localStorage.北京0129d="1";
+
+    localStorage.北京0130i="126";
+    localStorage.北京0130s="0";
+    localStorage.北京0130c="5";
+    localStorage.北京0130d="1";
+
+    localStorage.北京0131i="150";
+    localStorage.北京0131s="0";
+    localStorage.北京0131c="5";
+    localStorage.北京0131d="0";
+
+    localStorage.北京0201i="173";
+    localStorage.北京0201s="0";
+    localStorage.北京0201c="9";
+    localStorage.北京0201d="1";
+
+    localStorage.北京0202i="181";
+    localStorage.北京0202s="0";
+    localStorage.北京0202c="9";
+    localStorage.北京0202d="1";
+
+    //这是重庆的数据
+    localStorage.重庆0119i="0";
+    localStorage.重庆0119s="0";
+    localStorage.重庆0119c="0";
+    localStorage.重庆0119d="0";
+
+    localStorage.重庆0120i="0";
+    localStorage.重庆0120s="0";
+    localStorage.重庆0120c="0";
+    localStorage.重庆0120d="0";
+
+    localStorage.重庆0121i="5";
+    localStorage.重庆0121s="0";
+    localStorage.重庆0121c="0";
+    localStorage.重庆0121d="0";
+
+    localStorage.重庆0122i="6";
+    localStorage.重庆0122s="13";
+    localStorage.重庆0122c="0";
+    localStorage.重庆0122d="0";
+
+    localStorage.重庆0123i="9";
+    localStorage.重庆0123s="13";
+    localStorage.重庆0123c="0";
+    localStorage.重庆0123d="0";
+
+    localStorage.重庆0124i="27";
+    localStorage.重庆0124s="13";
+    localStorage.重庆0124c="0";
+    localStorage.重庆0124d="0";
+
+    localStorage.重庆0125i="57";
+    localStorage.重庆0125s="13";
+    localStorage.重庆0125c="0";
+    localStorage.重庆0125d="0";
+
+    localStorage.重庆0126i="75";
+    localStorage.重庆0126s="13";
+    localStorage.重庆0126c="0";
+    localStorage.重庆0126d="0";
+
+    localStorage.重庆0127i="110";
+    localStorage.重庆0127s="13";
+    localStorage.重庆0127c="0";
+    localStorage.重庆0127d="0";
+
+    localStorage.重庆0128i="110";
+    localStorage.重庆0128s="13";
+    localStorage.重庆0128c="0";
+    localStorage.重庆0128d="0";
+
+    localStorage.重庆0129i="132";
+    localStorage.重庆0129s="13";
+    localStorage.重庆0129c="0";
+    localStorage.重庆0129d="0";
+
+    localStorage.重庆0130i="147";
+    localStorage.重庆0130s="13";
+    localStorage.重庆0130c="0";
+    localStorage.重庆0130d="0";
+
+    localStorage.重庆0131i="164";
+    localStorage.重庆0131s="13";
+    localStorage.重庆0131c="1";
+    localStorage.重庆0131d="0";
+
+    localStorage.重庆0201i="205";
+    localStorage.重庆0201s="13";
+    localStorage.重庆0201c="1";
+    localStorage.重庆0201d="0";
+
+    localStorage.重庆0202i="236";
+    localStorage.重庆0202s="13";
+    localStorage.重庆0202c="3";
+    localStorage.重庆0202d="1";
+
+
+
+
+
+    //江西
+    localStorage.江西0119i="0";
+    localStorage.江西0119s="0";
+    localStorage.江西0119c="0";
+    localStorage.江西0119d="0";
+
+    localStorage.江西0120i="0";
+    localStorage.江西0120s="0";
+    localStorage.江西0120c="0";
+    localStorage.江西0120d="0";
+
+    localStorage.江西0121i="0";
+    localStorage.江西0121s="0";
+    localStorage.江西0121c="0";
+    localStorage.江西0121d="0";
+
+    localStorage.江西0122i="2";
+    localStorage.江西0122s="0";
+    localStorage.江西0122c="0";
+    localStorage.江西0122d="0";
+
+    localStorage.江西0123i="3";
+    localStorage.江西0123s="0";
+    localStorage.江西0123c="0";
+    localStorage.江西0123d="0";
+
+    localStorage.江西0124i="7";
+    localStorage.江西0124s="0";
+    localStorage.江西0124c="0";
+    localStorage.江西0124d="0";
+
+    localStorage.江西0125i="18";
+    localStorage.江西0125s="0";
+    localStorage.江西0125c="0";
+    localStorage.江西0125d="0";
+
+    localStorage.江西0126i="36";
+    localStorage.江西0126s="0";
+    localStorage.江西0126c="0";
+    localStorage.江西0126d="0";
+
+    localStorage.江西0127i="48";
+    localStorage.江西0127s="0";
+    localStorage.江西0127c="0";
+    localStorage.江西0127d="0";
+
+    localStorage.江西0128i="70";
+    localStorage.江西0128s="0";
+    localStorage.江西0128c="2";
+    localStorage.江西0128d="0";
+
+    localStorage.江西0129i="106";
+    localStorage.江西0129s="0";
+    localStorage.江西0129c="3";
+    localStorage.江西0129d="0";
+
+    localStorage.江西0130i="159";
+    localStorage.江西0130s="0";
+    localStorage.江西0130c="3";
+    localStorage.江西0130d="0";
+
+    localStorage.江西0131i="233";
+    localStorage.江西0131s="0";
+    localStorage.江西0131c="7";
+    localStorage.江西0131d="0";
+
+    localStorage.江西0201i="278";
+    localStorage.江西0201s="0";
+    localStorage.江西0201c="9";
+    localStorage.江西0201d="0";
+
+    localStorage.江西0202i="324";
+    localStorage.江西0202s="0";
+    localStorage.江西0202c="10";
+    localStorage.江西0202d="0";
+
+    //辽宁
+    localStorage.辽宁0119i="0";
+    localStorage.辽宁0119s="0";
+    localStorage.辽宁0119c="0";
+    localStorage.辽宁0119d="0";
+
+    localStorage.辽宁0120i="0";
+    localStorage.辽宁0120s="0";
+    localStorage.辽宁0120c="0";
+    localStorage.辽宁0120d="0";
+
+    localStorage.辽宁0121i="0";
+    localStorage.辽宁0121s="0";
+    localStorage.辽宁0121c="0";
+    localStorage.辽宁0121d="0";
+
+    localStorage.辽宁0122i="0";
+    localStorage.辽宁0122s="0";
+    localStorage.辽宁0122c="0";
+    localStorage.辽宁0122d="0";
+
+    localStorage.辽宁0123i="2";
+    localStorage.辽宁0123s="0";
+    localStorage.辽宁0123c="0";
+    localStorage.辽宁0123d="0";
+
+    localStorage.辽宁0124i="4";
+    localStorage.辽宁0124s="0";
+    localStorage.辽宁0124c="0";
+    localStorage.辽宁0124d="0";
+
+    localStorage.辽宁0125i="12";
+    localStorage.辽宁0125s="0";
+    localStorage.辽宁0125c="0";
+    localStorage.辽宁0125d="0";
+
+    localStorage.辽宁0126i="17";
+    localStorage.辽宁0126s="0";
+    localStorage.辽宁0126c="0";
+    localStorage.辽宁0126d="0";
+
+    localStorage.辽宁0127i="22";
+    localStorage.辽宁0127s="0";
+    localStorage.辽宁0127c="0";
+    localStorage.辽宁0127d="0";
+
+    localStorage.辽宁0128i="27";
+    localStorage.辽宁0128s="0";
+    localStorage.辽宁0128c="0";
+    localStorage.辽宁0128d="0";
+
+    localStorage.辽宁0129i="36";
+    localStorage.辽宁0129s="0";
+    localStorage.辽宁0129c="3";
+    localStorage.辽宁0129d="0";
+
+    localStorage.辽宁0130i="38";
+    localStorage.辽宁0130s="0";
+    localStorage.辽宁0130c="3";
+    localStorage.辽宁0130d="0";
+
+    localStorage.辽宁0131i="44";
+    localStorage.辽宁0131s="0";
+    localStorage.辽宁0131c="1";
+    localStorage.辽宁0131d="0";
+
+    localStorage.辽宁0201i="59";
+    localStorage.辽宁0201s="0";
+    localStorage.辽宁0201c="1";
+    localStorage.辽宁0201d="0";
+
+    localStorage.辽宁0202i="63";
+    localStorage.辽宁0202s="0";
+    localStorage.辽宁0202c="1";
+    localStorage.辽宁0202d="0";
+
+    //内蒙古
+    localStorage.内蒙古0119i="0";
+    localStorage.内蒙古0119s="0";
+    localStorage.内蒙古0119c="0";
+    localStorage.内蒙古0119d="0";
+
+    localStorage.内蒙古0120i="0";
+    localStorage.内蒙古0120s="0";
+    localStorage.内蒙古0120c="0";
+    localStorage.内蒙古0120d="0";
+
+    localStorage.内蒙古0121i="0";
+    localStorage.内蒙古0121s="0";
+    localStorage.内蒙古0121c="0";
+    localStorage.内蒙古0121d="0";
+
+    localStorage.内蒙古0122i="0";
+    localStorage.内蒙古0122s="0";
+    localStorage.内蒙古0122c="0";
+    localStorage.内蒙古0122d="0";
+
+    localStorage.内蒙古0123i="0";
+    localStorage.内蒙古0123s="0";
+    localStorage.内蒙古0123c="0";
+    localStorage.内蒙古0123d="0";
+
+    localStorage.内蒙古0124i="1";
+    localStorage.内蒙古0124s="2";
+    localStorage.内蒙古0124c="0";
+    localStorage.内蒙古0124d="0";
+
+    localStorage.内蒙古0125i="2";
+    localStorage.内蒙古0125s="5";
+    localStorage.内蒙古0125c="0";
+    localStorage.内蒙古0125d="0";
+
+    localStorage.内蒙古0126i="7";
+    localStorage.内蒙古0126s="0";
+    localStorage.内蒙古0126c="0";
+    localStorage.内蒙古0126d="0";
+
+    localStorage.内蒙古0127i="11";
+    localStorage.内蒙古0127s="1";
+    localStorage.内蒙古0127c="0";
+    localStorage.内蒙古0127d="0";
+
+    localStorage.内蒙古0128i="13";
+    localStorage.内蒙古0128s="2";
+    localStorage.内蒙古0128c="0";
+    localStorage.内蒙古0128d="0";
+
+    localStorage.内蒙古0129i="16";
+    localStorage.内蒙古0129s="2";
+    localStorage.内蒙古0129c="0";
+    localStorage.内蒙古0129d="0";
+
+    localStorage.内蒙古0130i="18";
+    localStorage.内蒙古0130s="3";
+    localStorage.内蒙古0130c="0";
+    localStorage.内蒙古0130d="0";
+
+    localStorage.内蒙古0131i="20";
+    localStorage.内蒙古0131s="4";
+    localStorage.内蒙古0131c="0";
+    localStorage.内蒙古0131d="0";
+
+    localStorage.内蒙古0201i="22";
+    localStorage.内蒙古0201s="9";
+    localStorage.内蒙古0201c="1";
+    localStorage.内蒙古0201d="0";
+
+    localStorage.内蒙古0202i="26";
+    localStorage.内蒙古0202s="13";
+    localStorage.内蒙古0202c="1";
+    localStorage.内蒙古0202d="0";
+
+    //宁夏
+    localStorage.宁夏0119i="0";
+    localStorage.宁夏0119s="0";
+    localStorage.宁夏0119c="0";
+    localStorage.宁夏0119d="0";
+
+    localStorage.宁夏0120i="0";
+    localStorage.宁夏0120s="0";
+    localStorage.宁夏0120c="0";
+    localStorage.宁夏0120d="0";
+
+    localStorage.宁夏0121i="0";
+    localStorage.宁夏0121s="0";
+    localStorage.宁夏0121c="0";
+    localStorage.宁夏0121d="0";
+
+    localStorage.宁夏0122i="0";
+    localStorage.宁夏0122s="0";
+    localStorage.宁夏0122c="0";
+    localStorage.宁夏0122d="0";
+
+    localStorage.宁夏0123i="1";
+    localStorage.宁夏0123s="0";
+    localStorage.宁夏0123c="0";
+    localStorage.宁夏0123d="0";
+
+    localStorage.宁夏0124i="2";
+    localStorage.宁夏0124s="1";
+    localStorage.宁夏0124c="0";
+    localStorage.宁夏0124d="0";
+
+    localStorage.宁夏0125i="3";
+    localStorage.宁夏0125s="1";
+    localStorage.宁夏0125c="0";
+    localStorage.宁夏0125d="0";
+
+    localStorage.宁夏0126i="4";
+    localStorage.宁夏0126s="1";
+    localStorage.宁夏0126c="0";
+    localStorage.宁夏0126d="0";
+
+    localStorage.宁夏0127i="7";
+    localStorage.宁夏0127s="0";
+    localStorage.宁夏0127c="0";
+    localStorage.宁夏0127d="0";
+
+    localStorage.宁夏0128i="11";
+    localStorage.宁夏0128s="2";
+    localStorage.宁夏0128c="0";
+    localStorage.宁夏0128d="0";
+
+    localStorage.宁夏0129i="12";
+    localStorage.宁夏0129s="2";
+    localStorage.宁夏0129c="0";
+    localStorage.宁夏0129d="0";
+
+    localStorage.宁夏0130i="17";
+    localStorage.宁夏0130s="1";
+    localStorage.宁夏0130c="0";
+    localStorage.宁夏0130d="0";
+
+    localStorage.宁夏0131i="21";
+    localStorage.宁夏0131s="41";
+    localStorage.宁夏0131c="0";
+    localStorage.宁夏0131d="0";
+
+    localStorage.宁夏0201i="26";
+    localStorage.宁夏0201s="1";
+    localStorage.宁夏0201c="1";
+    localStorage.宁夏0201d="0";
+
+    localStorage.宁夏0202i="28";
+    localStorage.宁夏0202s="8";
+    localStorage.宁夏0202c="1";
+    localStorage.宁夏0202d="0";
+
+    //青海
+    localStorage.青海0119i="0";
+    localStorage.青海0119s="0";
+    localStorage.青海0119c="0";
+    localStorage.青海0119d="0";
+
+    localStorage.青海0120i="0";
+    localStorage.青海0120s="0";
+    localStorage.青海0120c="0";
+    localStorage.青海0120d="0";
+
+    localStorage.青海0121i="0";
+    localStorage.青海0121s="0";
+    localStorage.青海0121c="0";
+    localStorage.青海0121d="0";
+
+    localStorage.青海0122i="0";
+    localStorage.青海0122s="0";
+    localStorage.青海0122c="0";
+    localStorage.青海0122d="0";
+
+    localStorage.青海0123i="0";
+    localStorage.青海0123s="0";
+    localStorage.青海0123c="0";
+    localStorage.青海0123d="0";
+
+    localStorage.青海0124i="0";
+    localStorage.青海0124s="0";
+    localStorage.青海0124c="0";
+    localStorage.青海0124d="0";
+
+    localStorage.青海0125i="0";
+    localStorage.青海0125s="0";
+    localStorage.青海0125c="0";
+    localStorage.青海0125d="0";
+
+    localStorage.青海0126i="1";
+    localStorage.青海0126s="0";
+    localStorage.青海0126c="0";
+    localStorage.青海0126d="0";
+
+    localStorage.青海0127i="4";
+    localStorage.青海0127s="0";
+    localStorage.青海0127c="0";
+    localStorage.青海0127d="0";
+
+    localStorage.青海0128i="6";
+    localStorage.青海0128s="0";
+    localStorage.青海0128c="0";
+    localStorage.青海0128d="0";
+
+    localStorage.青海0129i="6";
+    localStorage.青海0129s="0";
+    localStorage.青海0129c="0";
+    localStorage.青海0129d="0";
+
+    localStorage.青海0130i="6";
+    localStorage.青海0130s="1";
+    localStorage.青海0130c="0";
+    localStorage.青海0130d="0";
+
+    localStorage.青海0131i="8";
+    localStorage.青海0131s="1";
+    localStorage.青海0131c="0";
+    localStorage.青海0131d="0";
+
+    localStorage.青海0201i="9";
+    localStorage.青海0201s="1";
+    localStorage.青海0201c="0";
+    localStorage.青海0201d="0";
+
+    localStorage.青海0202i="11";
+    localStorage.青海0202s="1";
+    localStorage.青海0202c="0";
+    localStorage.青海0202d="0";
